aaf8db64697eaa5344c7c77b8b81fb05604e4b58
Luana <ludamici@alumno.unlam.edu.ar>
2020-05-14 20:30:27 -0300

    Subo arreglos en concursante para que funcione bien calcularDistanciaTotal

--- src/competenciaCaber/Concursante.java
+++ src/competenciaCaber/Concursante.java
@@ -7,28 +7,30 @@ public class Concursante {
 	private double consistencia;
 	private double distanciaTotal;
 	private ArrayList<Lanzamiento> lanzamientos;
-	
+
 	public Concursante(int numeroDeConcursante) {
 		this.numeroDeConcursante = numeroDeConcursante;
 		lanzamientos = new ArrayList<Lanzamiento>();
 	}
-	
-	/*public boolean validar() {
-		
-	}*/
-	
+
+	/*
+	 * public boolean validar() {
+	 * 
+	 * }
+	 */
+
 	public double calcularConsistencia() {
-		
+
 		return consistencia;
 	}
-	
+
 	public double calcularDistanciaTotal() {
 		double distanciaTotal = 0;
 		for (Lanzamiento lanzamiento : lanzamientos) {
 			distanciaTotal += lanzamiento.calcularDistanciaFinal();
 		}
 		return distanciaTotal;
-		
+
 	}
 
 	public int getNumeroDeConcursante() {
@@ -48,13 +50,13 @@ public class Concursante {
 	}
 
 	public double getDistanciaTotal() {
-		return distanciaTotal;
+		return calcularDistanciaTotal();
 	}
 
 	public void setDistanciaTotal(double distanciaTotal) {
 		this.distanciaTotal = distanciaTotal;
 	}
-	
+
 	public ArrayList<Lanzamiento> getLanzamientos() {
 		return lanzamientos;
 	}
